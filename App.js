579de5eb012ffeadff5e2f3351fbb7d413a7dcc3
mark <markpackham1@gmail.com>
2020-12-01 21:07:02 +0000

    position bird, https://youtu.be/dhpjjAxKbHE?t=935

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, View, Dimensions } from "react-native";
+import Bird from "./components/Bird";
 
 export default function App() {
+  const screenWidth = Dimensions.get("screen").width;
+  const screenHeigh = Dimensions.get("screen").height;
+  // the bottom left of our bird
+  const birdLeft = screenWidth / 2;
+  const [BirdBottom, setBirdBottom] = useState(screenHeigh / 2);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Bird />
     </View>
   );
 }
@@ -14,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/Bird.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View } from "react-native";
+
+const Bird = () => {
+  return (
+    <View
+      style={{
+        position: "absolute",
+        backgroundColor: "blue",
+        width: "50px",
+        height: "60px",
+      }}
+    />
+  );
+};
+
+export default Bird;
