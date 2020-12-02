a11003ff567e8bd0f3218ee7b87489cd82bc1c8c
mark <markpackham1@gmail.com>
2020-12-02 13:27:31 +0000

    apply gravity, https://youtu.be/dhpjjAxKbHE?t=1410

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Dimensions } from "react-native";
 import Bird from "./components/Bird";
 
@@ -8,12 +8,28 @@ export default function App() {
   const screenHeigh = Dimensions.get("screen").height;
   // the bottom left of our bird
   const birdLeft = screenWidth / 2;
-  const [BirdBottom, setBirdBottom] = useState(screenHeigh / 2);
+  const [birdBottom, setBirdBottom] = useState(screenHeigh / 2);
+  const gravity = 3;
+  let gameTimerId;
+
+  // start bird falling
+  useEffect(() => {
+    if (birdBottom > 0) {
+      gameTimerId = setInterval(() => {
+        // keep dropping birdBottom by 3 till it hits 0
+        setBirdBottom((birdBottom) => birdBottom - gravity);
+      }, 30);
+
+      return () => {
+        clearInterval(gameTimerId);
+      };
+    }
+  }, [birdBottom]);
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Bird />
+      <Bird birdBottom={birdBottom} birdLeft={birdLeft} />
     </View>
   );
 }
